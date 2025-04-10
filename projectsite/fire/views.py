95a402b66b9487d529401fdced199f9cd1d75218
joshISIP <202080065@psu.palawan.edu.ph>
2025-04-10 14:44:42 +0800

    fire stations map

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -10,11 +10,13 @@ class HomePageView(ListView):
 
 
 def map_station(request):
-    fire_station = FireStation.objects.values('name', 'latitude', 'longitude')
+    fireStations = FireStation.objects.values('name', 'latitude', 'longitude')
 
-    for fs in fire_stations:
+    for fs in fireStations:
         fs['latitude'] = float(fs['latitude'])
         fs['longitude'] = float(fs['longitude'])
+
+    fire_station_list = list(fireStations)
 
     context = {
         'fire_station': fire_station_list,  
