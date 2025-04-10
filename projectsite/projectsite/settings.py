2e2a15c32bb90e8cd1c3ec172c89a2df3e5de2e8
joshISIP <202080065@psu.palawan.edu.ph>
2025-04-10 15:00:35 +0800

    added forms.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # global /static/ folder
+]
 
 
 # Default primary key field type
